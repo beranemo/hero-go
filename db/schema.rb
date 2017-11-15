7e76d1847ba0068776055ee0a1ca822311b29d56
beranemo <beranemo@gmail.com>
2017-11-16 01:20:36 +0800

    create chatacter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115160018) do
+ActiveRecord::Schema.define(version: 20171115171924) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "heros", force: :cascade do |t|
     t.string "no"
